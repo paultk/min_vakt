8f89fe22884c9babbc294612bb87591b1a094188
Paul <paultk@stud.ntnu.no>
2017-01-10 10:47:24 +0100

    Initial commit

---
+++ src/main/java/com/example/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by paul on 10.01.17.
+ */
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
