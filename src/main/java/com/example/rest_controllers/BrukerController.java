6c7b86615ed66f71a9acd6e514881c4efa6a906d
Quinky <jenstk@gmail.com>
2017-01-11 09:10:23 +0100

    REST-package

---
+++ src/main/java/com/example/rest_controllers/BrukerController.java
@@ -0,0 +1,16 @@
+package com.example.rest_controllers;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Jens on 11-Jan-17.
+ */
+/*@Controller
+public class BrukerController {
+	@RequestMapping("/bruker/getbrukere")
+	*//*public String getBrukere() {
+
+	}*//*
+}*/
