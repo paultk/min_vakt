f10b3b97ff4695f3355d34de2292d7f8a4ffd1ef
Håkon <haakbe@student.hist.no>
2017-01-11 13:39:38 +0100

    kodet stilling sqlQueries

--- src/main/java/com/example/sql_folder/SqlQueries.java
+++ src/main/java/com/example/sql_folder/SqlQueries.java
@@ -198,6 +198,90 @@ public class SqlQueries extends DBConnection {
 
     /*
     *
+    * STILLING
+    *
+    */
+
+    public Stilling selectStilling(int stilling_id){
+
+        try{
+            String selectSql = "SELECT beskrivelse FROM stilling WHERE stilling_id = ?";
+            selectQuery = connection.prepareStatement(selectSql);
+            selectQuery.setInt(1, stilling_id);
+
+            ResultSet res = selectQuery.executeQuery();
+
+            if (!res.next()) return null;
+
+            String beskrivelse = res.getString(1);
+
+            return new Stilling(stilling_id, beskrivelse);
+
+        } catch (SQLException sqlE) {
+            sqlE.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    public boolean updateStilling(Stilling stilling){
+
+        try {
+            String sql = "UPDATE stilling SET beskrivelse = ? WHERE stilling_id = ?";
+            PreparedStatement updateQuery = connection.prepareStatement(sql);
+            updateQuery.setString(1, stilling.getBeskrivelse());
+            updateQuery.setInt(2, stilling.getStillingId());
+            if (updateQuery.executeUpdate() == 1) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean insertStilling(Stilling newStilling){
+        try {
+            String sql = "INSERT INTO stilling(beskrivelse) VALUES(?);";
+            insertQuery = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            insertQuery.setString(1, newStilling.getBeskrivelse());
+            insertQuery.execute();
+            ResultSet res = insertQuery.getGeneratedKeys();
+            res.next();
+            newStilling.setStillingId(res.getInt(1));
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    public boolean deleteStilling(Stilling stilling){
+        try {
+
+            String sql = "DELETE FROM stilling WHERE stilling_id = ?";
+            PreparedStatement deleteQuery = connection.prepareStatement(sql);
+            deleteQuery.setInt(1, stilling.getStillingId());
+            if (deleteQuery.executeUpdate() == 1) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /*
+    *
+    * PASSORD
+    *
+    */
+
+
+    /*
+    *
     * FRAVAER
     *
     */
