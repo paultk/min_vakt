34e787931b1498824cac1b01a000e5af2faa292a
Quinky <jenstk@gmail.com>
2017-01-11 10:02:19 +0100

    SQL-kobling

---
+++ src/main/java/com/example/SQL.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.sql.*;
+import java.util.Arrays;
+
+/**
+ * Created by Jens on 11-Jan-17.
+ */
+public class SQL {
+
+	static Connection conn;
+	static boolean connect() {
+		String url = "jdbc:mysql://mysql.stud.iie.ntnu.no/g_scrum06";
+		String user = "g_scrum06";
+		String pass = "aF9SoPaP";
+		try {
+			conn = DriverManager.getConnection(url, user, pass);
+			if (conn.isValid(1)) {
+				return true;
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception {
+		SQL.connect();
+		PreparedStatement prep = SQL.conn.prepareStatement("SELECT fornavn FROM bruker");
+		ResultSet res = prep.executeQuery();
+		if (res.next()) {
+			System.out.println(res.getString(1));
+		}
+//		Array arr = res.getString(1);
+//		System.out.println(Arrays.toString((String[])arr.getArray()));
+	}
+}
--- src/main/java/com/example/rest_controllers/BrukerController.java
+++ src/main/java/com/example/rest_controllers/BrukerController.java
@@ -1,16 +1,18 @@
 package com.example.rest_controllers;
 
+import com.example.database_classes.Bruker;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Jens on 11-Jan-17.
  */
-/*@Controller
+/*@RestController
 public class BrukerController {
 	@RequestMapping("/bruker/getbrukere")
-	*//*public String getBrukere() {
+	public Bruker getBrukere() {
 
-	}*//*
+	}
 }*/
